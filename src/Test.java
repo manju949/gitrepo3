0d484528adefa1d5a90bc4fb40924ee64283fd06
mansi patel <146835821+manju949@users.noreply.github.com>
2024-02-13 12:33:26 +0000

    commit4

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("My name is mans!");
         System.out.println("hi,good morning");
+          System.out.println("hi,good");
+        
     }
 }
